28d8674e43a5e80eb066224ec0be43d677955258
Namraj <active99raj@gmail.com>
2025-09-15 15:33:53 +1000

    Using DOM Event to add nav bar

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+const navCenter = document.getElementById("nav");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log(` <ul class="nav-links">
+      <li><a class="nav-link" href="./">Home</a></li>
+      <li><a class="nav-link" href="./work.html">Work</a></li>
+      <li><a class="nav-link" href="./about.html">About</a></li>
+      <li><a class="nav-link" href="./contact.html">Contact</a></li>
+<ul>`);
+
+  navCenter.innerHTML = `<div class="nav-center">
+  <div class="nav-header">
+    <img src="./assets/logo.svg" alt="logo" class="logo" />
+    <button class="nav-toggle">
+      <i class="fas fa-bars"></i>
+    </button>
+  </div>
+
+  <div class="links-container">
+    <ul class="nav-links">
+      <li><a class="nav-link" href="./">Home</a></li>
+      <li><a class="nav-link" href="./work.html">Work</a></li>
+      <li><a class="nav-link" href="./about.html">About</a></li>
+      <li><a class="nav-link" href="./contact.html">Contact</a></li>
+
+      <!-- <li>
+              <a href="#">profile</a>
+            </li> -->
+    </ul>
+  </div>
+</div>`;
+});
+
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".nav-links");
 const linksContainer = document.querySelector(".links-container");
