6a4891081876b45ddc95dd48e6fc4e8599171189
Namraj <active99raj@gmail.com>
2025-09-16 13:15:28 +1000

    Rename: public to src

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.resolve(__dirname, "src")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/user", (req, res) => {
